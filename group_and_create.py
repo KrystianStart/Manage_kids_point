3112c51f554f9591ac27c910015552d3fa8ce61b
KrystianStart <134496459+KrystianStart@users.noreply.github.com>
2023-05-26 10:01:44 +0200

    Delete group_and_create.py

--- group_and_create.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-
-mydb = mysql.connector.connect(user='sql8619611',
-                               password='2QTInCNfQY',
-                               host='sql8.freesqldatabase.com',
-                               database='sql8619611',
-                               port='3306')
